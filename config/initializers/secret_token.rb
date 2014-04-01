3b51c8ed46760b8c4bf2c9031021bd2e66641763
Nico Te <tenicoclyde@yahoo.ca>
2014-04-01 17:30:57 -0500

    First commit for the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KicksAreSick::Application.config.secret_key_base = '02bb0a983ba293b33a536585078e625dc4b6483f5f0196027b4dcb8d556068fdb1f195b8005d6a99eb91c443febcf20441887cd5f8f988ad8853700a301029f9'
